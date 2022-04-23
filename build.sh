b1f5e2971b33278d3c4b652565d9bfed5fe6e8c4
mirshad <mirshadkc2k19@gmail.com>
2022-04-23 06:25:39 -0600

    changed author

--- build.sh
+++ build.sh
@@ -11,11 +11,11 @@ rm -rvf dist/*
 echo "Building applications..."
 ng build --base-href / || exit 1
 
-echo "Droping old apps from EC2"
-ssh root@3.142.145.171 'rm -rvf /var/www/html/*'
+echo "Droping old apps from server"
+ssh root@162.241.90.60 'rm -rvf /var/www/html/*'
 
-echo "Deploying application on EC2..."
-rsync -avzP dist/keyshell/* root@3.142.145.171:/var/www/html/
+echo "Deploying application on server..."
+rsync -avzP dist/keyshell/* root@162.241.90.60:/var/www/html/
 
 if [ $? -eq 0 ]; then
     echo "Deployed the apps successfully"
